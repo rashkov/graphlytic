75135ddfda31c7477e1bb2722b63e0b877e237cc
Michael Rashkovsky <mrashkovsky@gmail.com>
2019-09-19 23:26:18 -0400

    Add redis test code

--- src/main/scala/graphlytic.scala
+++ src/main/scala/graphlytic.scala
@@ -3,11 +3,18 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
 import wikipedia._
+import com.redis._
 
 object graphlytic {
   val appName = "Graphlytic"
   val inputFilepath = "hdfs://ec2-3-212-99-96.compute-1.amazonaws.com:9000/user/wikipedia.dat"
   val outputFilepath = "hdfs://ec2-3-212-99-96.compute-1.amazonaws.com:9000/user/wikiTerms.txt"
+  // def publishToRedis(){
+  //   val r = new RedisClient("10.0.0.8", 6379)
+  //   r.set("somekey", "someval");
+  //   println(r.get("somekey"))
+  // }
+
   def main(args: Array[String]) {
     // setup the Spark Context
     val conf = new SparkConf()
@@ -24,5 +31,6 @@ object graphlytic {
       (wiki)=>wiki.terms.map((term)=>(term, wiki.title))
     )
     wikiTermsPairRdd.saveAsTextFile(outputFilepath)
+    // publishToRedis()
   }
 }
