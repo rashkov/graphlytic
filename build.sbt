9206bf0e4945cc4cbf7dc0d066739e57348d6d99
Michael Rashkovsky <mrashkovsky@gmail.com>
2019-10-01 17:00:58 -0400

    Update comments and cleanup

--- build.sbt
+++ build.sbt
@@ -9,7 +9,6 @@ libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "2.4.0" % "provided",
   "org.apache.spark" %% "spark-sql" % "2.4.0" % "provided",
   "org.apache.spark" %% "spark-mllib" % "2.4.0" % "provided",
-  "net.debasishg" %% "redisclient" % "3.10",
   "com.databricks" %% "spark-xml" % "0.6.0"
 )
 
--- src/main/scala/graphlytic.scala
+++ src/main/scala/graphlytic.scala
@@ -7,44 +7,48 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.sql.expressions._
 import org.apache.spark.ml.feature.{RegexTokenizer, StopWordsRemover}
 import com.databricks.spark.xml._
-import com.redis._
 import java.time.Instant
 
 object graphlytic {
-  val appName = "Graphlytic"
   val timestamp = Instant.now.getEpochSecond.toString
   val bucketName = sys.env.get("bucket") match {
     case Some(name) => name
     case None => throw new Exception("Please define a \"bucket\" environment variable")
   }
-  // val inputFilepath = "file:///home/mike/wiki_foo.xml"
   val inputFilepath = s"s3a://${bucketName}/enwiki-20190901-pages-articles-multistream.xml"
-  //val inputFilepath = s"s3a://${bucketName}/wikipedia_part.xml"
   val outputFilepath = s"s3a://${bucketName}/${timestamp}_wiki_index"
 
   def main(args: Array[String]) {
     val spark = SparkSession.builder.getOrCreate()
-    import spark.implicits._ // Enable $ syntax
+    import spark.implicits._ // Enable $ syntax among other things
 
+    // Parse input XML by reading <page> tags
     val df = spark.read
       .option("rowTag", "page")
       .xml(inputFilepath)
 
+    // Split words using regexTokenizer
     val regexTokenizer = new RegexTokenizer()
       .setInputCol("text")
       .setOutputCol("all_terms")
-      .setPattern("\\W") // alternatively .setPattern("\\w+").setGaps(false)
+      .setPattern("\\W")
 
+    // Filter out english stopwords
     val remover = new StopWordsRemover()
       .setInputCol("all_terms")
       .setOutputCol("filtered_terms")
 
+    // Select article ID & text fields, remove any with null values
     val wikiText = df.select("id", "revision.text._VALUE")
       .as[(Long, String)]
       .toDF("id", "text")
       .filter($"id".isNotNull && $"text".isNotNull)
+
+    // Tokenize
     val wikiTerms = regexTokenizer
       .transform(wikiText)
+
+    // Remove stopwords and build index, then write it to parquet in S3
     val wikiTermsFiltered = remover
       .transform(wikiTerms)
       .drop("text", "all_terms")
@@ -52,7 +56,7 @@ object graphlytic {
       .flatMap { case (id, terms) => terms.map((term)=>(term, id)) }
       .toDF("term", "id")
       .groupBy("term")
-      .agg(collect_set(col("id")) as "ids")
+      .agg(collect_set(col("id")) as "ids") // De-duplicate IDs via collect_set
       .as[(String, Seq[Long])]
       .write.parquet(outputFilepath)
   }
